5788ba4354ce0327a30f50c86daa4b81420dde95
Ahmed-Hany2 <ahmedhanypro2@gmail.com>
2025-08-12 18:06:23 +0300

    Question (2)

--- Assignment/Maths.cs
+++ Assignment/Maths.cs
@@ -8,23 +8,28 @@ namespace Assignment
 {
     public class Maths
     {
-        public double Add(double a, double b)
+        private static int operationCount = 0;
+        public static double Add(double a, double b)
         {
+            operationCount++;
             return a + b;
         }
 
-        public double Subtract(double a, double b)
+        public static double Subtract(double a, double b)
         {
+            operationCount++;
             return a - b;
         }
 
-        public double Multiply(double a, double b)
+        public static double Multiply(double a, double b)
         {
+            operationCount++;
             return a * b;
         }
 
-        public double Divide(double a, double b)
+        public static double Divide(double a, double b)
         {
+            operationCount++;
             if (b == 0)
             {
                 Console.WriteLine("Division by zero isnot valid");
@@ -33,19 +38,26 @@ namespace Assignment
             return a / b;
         }
 
-        public double Power(double a, double b)
+        public static double Power(double a, double b)
         {
+            operationCount++;
             return Math.Pow(a, b);
         }
 
-        public double Average(double[] numbers)
+        public static double Average(double[] numbers)
         {
+            operationCount++;
             double sum = 0;
             foreach (double num in numbers)
             {
                 sum += num;
             }
             return sum / numbers.Length;
+        }
+
+        public static void ShowOperationCount()
+        {
+            Console.WriteLine($"Total operations performed: {operationCount}");
         }
     }
 }
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,15 +4,15 @@
     {
         static void Main(string[] args)
         {
-            // Question (1)
-            Maths maths = new Maths();
+            // Question (1, 2)
 
-            Console.WriteLine("Add: " + maths.Add(5, 6));
-            Console.WriteLine("Subtract: " + maths.Subtract(5, 3));
-            Console.WriteLine("Multiply: " + maths.Multiply(8, 9));
-            Console.WriteLine("Divide: " + maths.Divide(50, 10));
-            Console.WriteLine("Power: " + maths.Power(2, 3));
-            Console.WriteLine("Average: " + maths.Average(  [2, 4, 6, 8 ]  ));
+            Console.WriteLine("Add: " + Maths.Add(5, 6));
+            Console.WriteLine("Subtract: " + Maths.Subtract(5, 3));
+            Console.WriteLine("Multiply: " + Maths.Multiply(8, 9));
+            Console.WriteLine("Divide: " + Maths.Divide(50, 10));
+            Console.WriteLine("Power: " + Maths.Power(2, 3));
+            Console.WriteLine("Average: " + Maths.Average(  [2, 4, 6, 8 ]  ));
+            Maths.ShowOperationCount();
 
 
 
