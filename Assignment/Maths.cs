362934c94f324b69872feb9802e9bec223b6492e
Ahmed-Hany2 <ahmedhanypro2@gmail.com>
2025-08-12 18:02:02 +0300

    Question (1)

---
+++ Assignment/Maths.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public class Maths
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                Console.WriteLine("Division by zero isnot valid");
+                return double.NaN;
+            }
+            return a / b;
+        }
+
+        public double Power(double a, double b)
+        {
+            return Math.Pow(a, b);
+        }
+
+        public double Average(double[] numbers)
+        {
+            double sum = 0;
+            foreach (double num in numbers)
+            {
+                sum += num;
+            }
+            return sum / numbers.Length;
+        }
+    }
+}
--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            
+            // Question (1)
+            Maths maths = new Maths();
+
+            Console.WriteLine("Add: " + maths.Add(5, 6));
+            Console.WriteLine("Subtract: " + maths.Subtract(5, 3));
+            Console.WriteLine("Multiply: " + maths.Multiply(8, 9));
+            Console.WriteLine("Divide: " + maths.Divide(50, 10));
+            Console.WriteLine("Power: " + maths.Power(2, 3));
+            Console.WriteLine("Average: " + maths.Average(  [2, 4, 6, 8 ]  ));
+
+
+
         }
     }
 }
